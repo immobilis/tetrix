8efe7bdead9c099f7bf8f7b8394dbbe880454f2d
immobilis <immobilis.logic@gmail.com>
2014-04-06 22:40:37 -0500

    Added builder key controls and collisions

--- main.lua
+++ main.lua
@@ -13,3 +13,10 @@ end
 function love.keypressed( key, isrepeat )
 	curState:keypressed( key, isrepeat )
 end
+
+function round( num )
+	local x = 0
+	if num<0 then x=-.5 else x=.5 end
+	local integer = math.modf(num+x)
+	return integer
+end
--- states/builder.lua
+++ states/builder.lua
@@ -8,6 +8,7 @@ function curState:init()
 	local piecesdata = require("data/pieces")
 	self.pieces = piecesdata.pieces
 	self.colors = piecesdata.colors
+	curState.rotatePiece = piecesdata.rotatePiece
 
 	-- remove stray data, but remember
 	-- loaded packages
@@ -32,11 +33,103 @@ function curState:init()
 	self.next2 = self.pieces[math.random( 7 )]
 	self.xpos = math.random( 20 - #self.current - 1 )
 	self.ypos = -5
+	self.xvel = 0
+	self.yvel = 2
+	self.yvmax = 2
 end
 
 function curState:update( dt )
 	-- apply gravity
-	self.ypos = self.ypos + 2 * dt
+	self.ypos = self.ypos + self.yvel * dt
+
+	-- yvel will remain at max if it doesn't hit anything
+	self.yvel = self.yvmax
+	-- check collision with other pieces
+	-- and bottom of board
+	for x, r in ipairs( self.current ) do
+		for y, b in ipairs( r ) do
+			-- check to see if this is a block and
+			-- if it is on the bottom of the piece
+			-- and if it is on the board
+			if b ~= 0 and (not r[y + 1] or r[y + 1] == 0) and self.ypos + y - 1 >= 0 then
+				-- if there is something below it then
+				if self.board[math.ceil( self.xpos ) + x - 1][math.floor( self.ypos ) + y] ~= 0 or self.board[math.floor( self.xpos ) + x - 1][math.floor( self.ypos ) + y] ~= 0 then
+					-- put the piece back in a cube
+					self.ypos = math.floor(self.ypos)
+					-- stop the piece
+					self.yvel = 0
+					break -- out of yloop
+				end -- belowif
+			end -- bottomif
+		end -- yloop
+		-- easy breakage when piece is stopped
+		if self.yvel == 0 then break end
+	end -- xloop
+
+	-- apply x velocity
+	self.xpos = self.xpos + self.xvel * dt
+
+	-- check collision with other pieces
+	-- and sides of board
+	if self.xvel < 0 then
+		for x, r in ipairs( self.current ) do
+			for y, b in ipairs( r ) do
+				-- check to see if this is a block and
+				-- if it is on the left side of the piece
+				-- and if it is on the board
+				if b ~= 0 and ( not self.current[x - 1] or self.current[x - 1][y] == 0) and self.ypos + y - 1 >= 0 then
+					-- if there is something to the left of it then
+					if not self.board[math.floor(self.xpos) + x - 1] or self.board[math.floor(self.xpos) + x - 1][math.ceil(self.ypos) + y - 1] ~= 0 or self.board[math.floor(self.xpos) + x - 1][math.floor(self.ypos) + y - 1] ~= 0 then
+						self.xpos = math.ceil(self.xpos)
+						self.xvel = 0
+						break -- out of yloop
+					end -- leftif
+				end -- sideif
+			end -- yloop
+			-- easy breakage when piece is stopped
+			if self.xvel == 0 then break end
+		end -- xloop
+	elseif self.xvel > 0 then
+		for x, r in ipairs( self.current ) do
+			for y, b in ipairs( r ) do
+				-- check to see if this is a block and
+				-- if it is on the right side of the piece
+				-- and if it is on the board
+				if b ~= 0 and ( not self.current[x + 1] or self.current[x + 1][y] == 0) and self.ypos + y - 1 >= 0 then
+					-- if there is something to the right of it then
+					if not self.board[math.ceil(self.xpos) + x - 1] or self.board[math.ceil(self.xpos) + x - 1][math.ceil(self.ypos) + y - 1] ~= 0 or self.board[math.ceil(self.xpos) + x - 1][math.floor(self.ypos) + y - 1] ~= 0 then
+						self.xpos = math.floor(self.xpos)
+						self.xvel = 0
+						break -- out of yloop
+					end -- leftif
+				end -- sideif
+			end -- yloop
+			-- easy breakage when piece is stopped
+			if self.xvel == 0 then break end
+		end -- xloop
+	end
+
+	-- if the piece has stopped then
+	if self.xvel == 0 and self.yvel == 0 then
+		-- put the piece on the board
+		for x, r in ipairs( self.current ) do
+			for y, b in ipairs( r ) do
+				if b ~= 0 then
+					self.board[round(self.xpos) + x - 1][round(self.ypos) + y - 1] = b
+				end
+			end
+		end
+
+		-- move up the queue, generate another piece, and
+		-- place it above the board
+		self.current = self.next1
+		self.next1 = self.next2
+		self.next2 = self.pieces[math.random( 7 )]
+		self.xpos = math.random( 20 - #self.current - 1 )
+		self.ypos = -5
+		self.xvel = 0
+		self.yvel = 2
+	end
 end
 
 function curState:draw()
@@ -53,6 +146,18 @@ function curState:draw()
 			love.graphics.setColor( self.colors[b] )
 			love.graphics.draw( self.block, (self.xpos * 20 - 20) + (x * 20 - 20), (self.ypos * 20 - 20) + (y * 20 - 20) )
 		end
+	end
+end
+
+function curState:keypressed( key, isrepeat )
+	if key == "left" then
+		self.xvel = -6
+	elseif key == "right" then
+		self.xvel = 6
+	elseif key == "down" then
+		self.yvmax = 8
+	elseif key == "up" then
+		self.yvmax = 2
 	end
 end
 
